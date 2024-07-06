b4926721fc6c73090f02f813b216e5eb23341928
Dove9 <khb532@naver.com>
2024-07-07 00:35:29 +0900

    -

--- Source/PuzzlePlatforms/PuzzleGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzleGameInstance.cpp
@@ -2,4 +2,38 @@
 
 
 #include "PuzzleGameInstance.h"
+#include "Engine/Engine.h"
 
+
+UPuzzleGameInstance::UPuzzleGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+    UE_LOG(LogTemp, Warning, TEXT("Constructor has called"));
+}
+
+void UPuzzleGameInstance::Init()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Init has called"));
+}
+
+void UPuzzleGameInstance::Host(void)
+{
+    UEngine* Engine = GetEngine();                                              // GameInstance::GetEngine()
+    if(Engine == nullptr)
+        return;
+    Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));      // #include Engine/Engine.h
+
+    UWorld* World = GetWorld();
+    if(World == nullptr)
+        return;
+
+    World->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen");               // UWorld::ServerTravel()
+}
+
+void UPuzzleGameInstance::Join(const FString& address)
+{
+    UEngine* Engine = GetEngine();
+    if(Engine != nullptr)
+    {
+        Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *address));
+    }
+}
--- Source/PuzzlePlatforms/PuzzleGameInstance.h
+++ Source/PuzzlePlatforms/PuzzleGameInstance.h
@@ -13,5 +13,25 @@ UCLASS()
 class PUZZLEPLATFORMS_API UPuzzleGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	UPuzzleGameInstance(const FObjectInitializer& ObjectInitializer);
+	virtual void Init();
+
+	/*
+	UFUNCTION에 인자로 Exec가 가능한 클래스들
+	@ PlayerControllers
+		- Possessed Pawns
+	@ HUD
+	@ Cheat Manager
+	@ GameMode
+	@ GameInstance
+	*/
+	UFUNCTION(Exec)		// Exec : 콘솔명령어, 플레이중 콘솔에서 사용할 명령어 생성
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString& address);
+
 	
 };
